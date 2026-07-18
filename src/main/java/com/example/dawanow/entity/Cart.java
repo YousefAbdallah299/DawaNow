964534dd45986e33d413605bdbaa5500945d1304
Sarah Sameh <sarahsameh7@gmail.com>
2026-07-18 14:26:41 +0300

    Add Cart Entity

---
+++ src/main/java/com/example/dawanow/entity/Cart.java
@@ -0,0 +1,34 @@
+package com.example.dawanow.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Cart {
+
+    public Cart(User user) {
+        this.user = user;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal totalPrice;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<CartItem> items;
+}
